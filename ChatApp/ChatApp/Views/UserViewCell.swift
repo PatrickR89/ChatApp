a25dda2f1888a96600f7d8bb609cb612abb6ac43
PatrickR <patrick@ruzman.net>
2023-02-28 19:19:56 +0100

    ChatApp - UserVIewCell
    - [ Add ] - design and animation to mark new active users

--- ChatApp/ChatApp/Views/UserViewCell.swift
+++ ChatApp/ChatApp/Views/UserViewCell.swift
@@ -23,10 +23,21 @@ class UserViewCell: UITableViewCell {
         contentView.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = model.username
+        label.textColor = UIConstants.accentColor
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
 
         NSLayoutConstraint.activate([
             label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
         ])
+
+        convertLabelToPassive()
+    }
+
+    func convertLabelToPassive() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            UIView.transition(with: self.label, duration: 0.5, options: .transitionCrossDissolve, animations: {self.label.textColor = UIConstants.lightColor}, completion: nil)
+        }
     }
 }
